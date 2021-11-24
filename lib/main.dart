da40ba005e332465f0c107d074c4b083ea751271
sonali jain <60497488+sonalijain1999@users.noreply.github.com>
2021-11-24 11:48:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,47 @@ class MyApp extends StatefulWidget {
   @override
   _State createState() => new _State();
 }
-
+String text="";
 class _State extends State<MyApp> {
+  final GlobalKey<ScaffoldState> ScaffoldKey=GlobalKey<ScaffoldState>();
+  void _onSubmitted(String _text)
+  { setState(() {
+    text=_text;
+  });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: ScaffoldKey,
       appBar:  AppBar(
         title: const Text('Assigment5'),
       ),
       body:  Container(
         padding:  EdgeInsets.all(32.0),
         child:  Column(
-          children: const <Widget>[
-             Text('This is a template code'),
-            TextField(),
+          children:  <Widget>[
+
+            TextField(
+              onChanged: _onSubmitted
+            ),
+            ElevatedButton(
+              onPressed: (){
+                final snackBar=SnackBar(
+                    content: Text(text),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+                child: Text("SnackBar"),
+
+            ),
+
+
           ],
         ),
+
       ),
-    );
+         );
   }
 }
